2f2efea7a9a204a9ce8330f44d7a6bbd60722749
Madlen <madlen@SoftRay.local>
2021-05-10 15:20:20 +0200

    Added global style and normalize

---
+++ src/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html{
+  box-sizing: border-box;
+  font-family: "Lato", sans-serif;
+
+}
+*, ::after, ::before {
+  box-sizing: inherit;
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+a{
+  color: inherit;
+  text-decoration: none;
+}
+`;
