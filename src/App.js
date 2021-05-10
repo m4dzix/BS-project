ec8852940e66a84d28c1fa64dd741e208d860801
Madlen <madlen@SoftRay.local>
2021-05-11 01:13:38 +0200

    Return from App Header and Logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import Navigation from "./Navigation/index";
+import Header from "./Header/index";
+import Logo from "./Logo/index";
 
 function App() {
-  return <Navigation />;
+  return (
+    <>
+      <Header />
+      <Logo />
+      <Navigation />
+    </>
+  );
 }
 
 export default App;
