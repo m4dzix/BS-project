00a624811c2f7a39a2bb74e4a71a594199d03807
Madlen <madlen@SoftRay.local>
2021-05-11 01:11:03 +0200

    Created Header with Title

---
+++ src/Header/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Title } from "./styled";
+
+const Header = () => <Title>Lorem ipsum dolor</Title>;
+
+export default Header;
---
+++ src/Header/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 50px;
+  max-width: 1200px;
+  margin: 0 auto;
+  font-weight: bold;
+  padding: 10px 30px;
+  color: ${({ theme }) => theme.colors.text};
+`;
