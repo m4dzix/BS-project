b5bf2ba81f8412650bd3f53146b88e48f7f6e1ab
Madlen <madlen@SoftRay.local>
2021-05-11 16:22:36 +0200

    Created array with images

---
+++ src/Slider/images/arrayImages.js
@@ -0,0 +1,19 @@
+import foto1 from "./foto1.jpg";
+import foto2 from "./foto2.jpg";
+import foto3 from "./foto3.jpg";
+import foto4 from "./foto4.jpg";
+import foto5 from "./foto5.jpg";
+import foto6 from "./foto6.jpg";
+import foto7 from "./foto7.jpg";
+import foto8 from "./foto8.jpg";
+
+export const arrayImages = [
+  foto1,
+  foto2,
+  foto3,
+  foto4,
+  foto5,
+  foto6,
+  foto7,
+  foto8,
+];
