be80aede6e5ac5012835c477353d2bb22aa710ee
Madlen <madlen@SoftRay.local>
2021-05-13 21:17:02 +0200

    Created objec containing the meeting schedule

---
+++ src/Schedule/meetingShedule.js
@@ -0,0 +1,76 @@
+export const meetingSchedule = {
+  native_languages: [
+    {
+      name: "BS-Nord",
+      day: ["Dienstag", "Sonntag"],
+      time: ["18:45 Uhr - 20:30 Uhr", "10:00 Uhr - 12:00 Uhr"],
+    },
+    {
+      name: "BS-Süd",
+      day: ["Mittwoch", "Sonntag"],
+      time: ["18:45 Uhr - 20:30 Uhr", "10:00 Uhr - 12:00 Uhr"],
+    },
+    {
+      name: "BS-West",
+      day: ["Donnerstag", "Sonntag"],
+      time: ["18:45 Uhr - 20:30 Uhr", "10:00 Uhr - 12:00 Uhr"],
+    },
+    {
+      name: "BS-Mitte",
+      day: ["Freitag", "Sonntag"],
+      time: ["18:45 Uhr - 20:30 Uhr", "10:00 Uhr - 12:00 Uhr"],
+    },
+  ],
+  foreign_languages: [
+    {
+      name: "BS-Polnisch",
+      language: "Polski",
+      day: ["Dienstag", "Sonntag"],
+      time: ["18:45 Uhr - 20:30 Uhr", "10:00 Uhr - 12:00 Uhr"],
+    },
+    {
+      name: "BS-Russisch",
+      language: "Pусский",
+      day: ["Mittwoch", "Sonntag"],
+      time: ["18:45 Uhr - 20:30 Uhr", "10:00 Uhr - 12:00 Uhr"],
+    },
+    {
+      name: "BS-Spanisch",
+      language: "Español",
+      day: ["Donnerstag", "Sonntag"],
+      time: ["18:45 Uhr - 20:30 Uhr", "10:00 Uhr - 12:00 Uhr"],
+    },
+    {
+      name: "BS-Türkisch",
+      language: "Türk",
+      day: ["Dienstag", "Sonntag"],
+      time: ["18:45 Uhr - 20:30 Uhr", "10:00 Uhr - 12:00 Uhr"],
+    },
+    {
+      name: "BS-Französisch",
+      language: "Français",
+      day: ["Mittwoch", "Sonntag"],
+      time: ["18:45 Uhr - 20:30 Uhr", "10:00 Uhr - 12:00 Uhr"],
+    },
+    {
+      name: "BS-Englisch",
+      language: "English",
+      day: ["Donnerstag", "Sonntag"],
+      time: ["18:45 Uhr - 20:30 Uhr", "10:00 Uhr - 12:00 Uhr"],
+    },
+    {
+      name: "BS-Chinesisch",
+      language: "中文繁體",
+      day: ["Freitag", "Sonntag"],
+      time: ["18:45 Uhr - 20:30 Uhr", "10:00 Uhr - 12:00 Uhr"],
+    },
+  ],
+  sign_languages: [
+    {
+      name: "Deutsche Gebärdensprache",
+      language: "Deutsche Gebärdensprache",
+      day: ["Freitag", "Sonntag"],
+      time: ["18:45 Uhr - 20:30 Uhr", "10:00 Uhr - 12:00 Uhr"],
+    },
+  ],
+};
