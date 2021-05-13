7caa8473772cdcc9b4a9a792554756b1dba50dc3
Madlen <madlen@SoftRay.local>
2021-05-13 21:18:39 +0200

    Created subpage Schedule

--- src/Schedule/index.js
+++ src/Schedule/index.js
@@ -1,9 +1,61 @@
 import React from "react";
 import { Section } from "../Section";
-import { Title } from "../Title";
+import { Article } from "./Article";
+import Tile from "./Tile/index";
+
+import { meetingSchedule } from "./meetingShedule";
 
 const Schedule = () => {
-  return <Section title={<Title title={"schedule Lorem Ipsum:"} />} />;
+  const nativeLanguages = meetingSchedule.native_languages;
+  const foreignLanguages = meetingSchedule.foreign_languages;
+  const signLanguages = meetingSchedule.sign_languages;
+  return (
+    <Section
+      title={"Zusammenkünfte"}
+      body={
+        <>
+          <Article
+            title={"Deutsche Sprache"}
+            body={nativeLanguages.map((meeting) => (
+              <Tile
+                key={meeting.name}
+                name={meeting.name}
+                hidden={true}
+                day={meeting.day}
+                time={meeting.time}
+              />
+            ))}
+          />
+          <Article
+            title={"Fremdsprachen"}
+            body={foreignLanguages.map((meeting) => (
+              <Tile
+                key={meeting.name}
+                name={meeting.name}
+                hidden={false}
+                language={meeting.language}
+                day={meeting.day}
+                time={meeting.time}
+              />
+            ))}
+          />
+          <Article
+            title={"Gebärdensprache"}
+            body={signLanguages.map((meeting) => (
+              <Tile
+                key={meeting.name}
+                name={meeting.name}
+                hidden={false}
+                language={meeting.language}
+                day={meeting.day}
+                time={meeting.time}
+              />
+            ))}
+          />
+        </>
+      }
+    />
+  );
 };
 
 export default Schedule;
