bb8c7bd79d0344b588a47931f67c0de0c461cb87
Madlen <madlen@SoftRay.local>
2021-05-13 21:15:42 +0200

    Deleted component Title

--- src/Title/index.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { Subheader } from "./styled";
-
-export const Title = ({ title }) => <Subheader>{title}</Subheader>;
--- src/Title/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Subheader = styled.h2`
-  color: ${({ theme }) => theme.colors.text};
-  font-size: 40px;
-  border-bottom: 3px solid ${({ theme }) => theme.colors.text};
-  padding: 10px;
-  text-transform: capitalize;
-`;
