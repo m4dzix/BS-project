6a775ef98fd278944487d8d13f2005a9264424ab
Madlen <madlen@SoftRay.local>
2021-05-14 13:06:48 +0200

    Created sample gallery

---
+++ src/features/Gallery/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "react-image-gallery/styles/css/image-gallery.css";
+import ImageGallery from "react-image-gallery";
+import Slider from "../../common/Slider";
+import { Section } from "../../common/Section";
+
+const images = [
+  {
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
+  },
+  {
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
+  },
+];
+
+const MyGallery = () => {
+  return (
+    <>
+      <Slider aboutUs={true} />
+      <Section
+        title={"Lorem ipsum dolor "}
+        body={images ? <ImageGallery items={images} /> : null}
+      />
+    </>
+  );
+};
+
+export default MyGallery;
--- src/features/Schedule/index.js
+++ src/features/Schedule/index.js
@@ -3,27 +3,31 @@ import { Section } from "../../common/Section";
 import { Article } from "./Article";
 import Tile from "./Tile/index";
 import { meetingSchedule } from "./meetingShedule";
+import Slider from "../../common/Slider";
 
 const Schedule = () => {
   return (
-    <Section
-      title={"Zusammenkünfte"}
-      body={meetingSchedule.map((meetings) => (
-        <Article
-          key={meetings.type}
-          title={meetings.type}
-          body={meetings.details.map((meeting) => (
-            <Tile
-              key={meeting.name}
-              name={meeting.name}
-              hidden={true}
-              day={meeting.day}
-              time={meeting.time}
-            />
-          ))}
-        />
-      ))}
-    />
+    <>
+      <Slider />;
+      <Section
+        title={"Zusammenkünfte"}
+        body={meetingSchedule.map((meetings) => (
+          <Article
+            key={meetings.type}
+            title={meetings.type}
+            body={meetings.details.map((meeting) => (
+              <Tile
+                key={meeting.name}
+                name={meeting.name}
+                hidden={true}
+                day={meeting.day}
+                time={meeting.time}
+              />
+            ))}
+          />
+        ))}
+      />
+    </>
   );
 };
 
