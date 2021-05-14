53671171474d10cbe6f94ac0f9526df7dc45186e
Madlen <madlen@SoftRay.local>
2021-05-14 10:14:57 +0200

    Created basic subpage aoutUs

---
+++ src/features/AboutUs/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Section } from "../../common/Section";
+import Article from "./Article";
+
+const AboutUs = () => (
+  <Section
+    title={"Lorem ipsum dolor sit amount"}
+    body={
+      <>
+        {" "}
+        <Article />
+      </>
+    }
+  ></Section>
+);
+
+export default AboutUs;
