97a98354ef8aba48e5935d5e1963d51538002921
Madlen <madlen@SoftRay.local>
2021-05-16 19:39:28 +0200

    Decreased logo size on mobile

--- src/common/Header/Logo/styled.js
+++ src/common/Header/Logo/styled.js
@@ -11,10 +11,16 @@ export const Square = styled.div`
   position: absolute;
   top: 10px;
   z-index: 1;
+  @media (max-width: 767px) {
+    transform: scale(0.8);
+  }
 `;
 export const Text = styled.p`
   color: ${({ theme }) => theme.colors.background};
   text-transform: uppercase;
   font-size: 80px;
   text-shadow: 0px 5px 4px ${({ theme }) => theme.colors.text};
+  @media (max-width: 767px) {
+    transform: scale(0.8);
+  }
 `;
