b65120564739e548dabca2ea03f3c8cc6e6e427f
Madlen <madlen@SoftRay.local>
2021-06-14 01:04:52 +0200

    Changed main color

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,7 @@
 export const theme = {
   colors: {
     white: "#FFFFFF",
-    mainColor: "#EFCA08",
+    mainColor: "#FCEEA8",
     text: "#1C1C1C",
     background: "#F7FAEF",
     hover: "#9A9A9C",
