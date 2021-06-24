fa150e663bab8c26185c359f954615a0453a66c9
Madlen <madlen@SoftRay.local>
2021-06-24 13:05:32 +0200

    Created footer

---
+++ src/common/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledFooter, Info } from "./styled";
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <Info>{"\u00A9"} copywright </Info>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
