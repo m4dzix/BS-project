980c3e91343f7783ebe4404a309a11c417ea0b31
Madlen <madlen@SoftRay.local>
2021-06-24 21:29:40 +0200

    Changed gallery button appearance

--- src/features/Build/styled.js
+++ src/features/Build/styled.js
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom";
 import { ArrowCircleRight } from "@styled-icons/heroicons-solid/ArrowCircleRight";
 
 export const StyledLink = styled(Link)`
-  color: ${({ theme }) => theme.colors.text};
-  font-size: 30px;
-  background-color: ${({ theme }) => theme.colors.mainColor};
-  padding: 10px;
-  margin: 30px;
-  margin-left: 0;
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 20px;
+  background-color: ${({ theme }) => theme.colors.text};
+  padding: 5px;
+  margin: 0px;
+  margin-right: 40px;
   text-align: center;
   border-radius: 5px;
+  width: 300px;
+  align-self: flex-end;
   &:hover {
-    letter-spacing: 3px;
+    filter: brightness(1.5);
+    transform: scale(1.1);
   }
 `;
 
