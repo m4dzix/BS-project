a07f597bd00b76bf6eebafc236aca0a3fbb97525
Madlen <madlen@SoftRay.local>
2021-06-24 11:19:39 +0200

    Added Login to Header

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -4,6 +4,7 @@ import Navigation from "./Navigation";
 import Title from "./Title";
 import { StyledHeader } from "./styled";
 import MenuHamburger from "./MenuHamburger";
+import Login from "./Login";
 
 const Header = () => {
   const [hide, setHide] = useState(true);
@@ -11,6 +12,7 @@ const Header = () => {
   return (
     <StyledHeader>
       <Logo />
+      <Login />
       <MenuHamburger hide={hide} toggleHide={toggleHide} />
       <Title title={"Lorem ipsum dolor"} />
       <Navigation hide={hide} toggleHide={toggleHide} />
