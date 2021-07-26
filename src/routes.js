b66d3727d9c3508fd65f8d464f5a45b11212d2ab
Madlen <madlen@SoftRay.local>
2021-07-26 20:23:02 +0200

    Created routes to booking subpages

--- src/routes.js
+++ src/routes.js
@@ -3,3 +3,6 @@ export const toAboutUs = () => "/aboutUs";
 export const toBuild = () => "/build";
 export const toGallery = () => "/gallery";
 export const toBooking = () => "/booking";
+export const toTrucks = () => "/trucks";
+export const toTrolley = () => "/trolley";
+export const toAccommodation = () => "/accommodation";
