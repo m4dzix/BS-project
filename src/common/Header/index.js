86392fd994722458d6b11f068dc46f12839aa6d8
Madlen <madlen@SoftRay.local>
2021-07-28 21:29:21 +0200

    Created Login Authentication  using Auth0

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
-import Logo from "./Logo";
 import Navigation from "./Navigation";
 import Title from "./Title";
 import { StyledHeader } from "./styled";
 import MenuHamburger from "./MenuHamburger";
-import Login from "./Login";
+import LoginButton from "../LoginButton";
 
 const Header = () => {
   const [hide, setHide] = useState(true);
   const toggleHide = () => setHide(!hide);
+
   return (
     <StyledHeader>
-      <Logo />
-      <Login />
+      <LoginButton />
       <MenuHamburger hide={hide} toggleHide={toggleHide} />
       <Title title={"Lorem ipsum dolor"} />
       <Navigation hide={hide} toggleHide={toggleHide} />
