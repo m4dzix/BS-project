13717f515057207256de5424a89e6c767354ed39
Madlen <madlen@SoftRay.local>
2021-08-10 22:41:09 +0200

    Increased the min.column width

--- src/features/Schedule/Article/styled.js
+++ src/features/Schedule/Article/styled.js
@@ -49,7 +49,7 @@ export const Wrapper = styled.div`
   grid-gap: 20px;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   @media (max-width: 767px) {
-    grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
   }
   ${(props) =>
     props.hide &&
