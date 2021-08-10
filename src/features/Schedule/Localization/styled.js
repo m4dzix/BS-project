ee2dbecf2a4f9debef372b85b8632ef8450d379f
Madlen <madlen@SoftRay.local>
2021-08-10 22:24:23 +0200

    Changed The arrangement of elements on mobile landscape orientation

--- src/features/Schedule/Localization/styled.js
+++ src/features/Schedule/Localization/styled.js
@@ -5,6 +5,10 @@ export const StyledLocalization = styled.div`
   display: grid;
   grid-template-columns: 1fr 400px;
   height: 400px;
+  @media (max-width: 767px) and (orientation: landscape) {
+    grid-template-columns: 1fr 1fr;
+    justify-content: center;
+  }
   @media (max-width: 767px) and (orientation: portrait) {
     grid-template-columns: none;
     grid-template-rows: 1fr 1fr;
@@ -23,25 +27,27 @@ export const List = styled.dl`
   @media (max-width: 767px) {
     padding: 10px;
   }
+  @media (max-width: 767px) and(orientation:portrait) {
+    margin: 0 auto;
+  }
 `;
 
 export const Key = styled.dt`
   font-size: 25px;
   font-weight: bold;
   padding: 10px;
-  @media (max-width: 767px) and (orientation: landscape) {
-    font-size: 20px;
+  @media (max-width: 767px) {
+    font-size: 18px;
+    padding: 5px;
   }
 `;
 
 export const Value = styled.dd`
   font-size: 22px;
   padding: 10px;
-  @media (max-width: 767px) and (orientation: landscape) {
-    font-size: 18px;
-  }
+
   @media (max-width: 767px) {
-    font-size: 18px;
+    font-size: 15px;
     padding: 0;
     margin: 10px;
   }
